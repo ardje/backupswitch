a4d2acbe6fdfc7a4d91bd896e7141c5e98586661
Ard van Breemen @ lenny <ard@kwaak.net>
2021-04-23 22:53:47 +0200

    Use ipv6 ll

---
+++ cons6.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+DATADIR=/etc/switches
+SWITCHLIST=$DATADIR/switches.txt
+THISDIR=$PWD
+while read sw ip class password;do
+myip="bogus"
+export SW="$sw"
+export IP="$ip"
+export CLASS="$class"
+export MYIP="$myip"
+export PASSWORD="$password"
+done < <( awk '/'"$1"'/ { print $1,$3,$5,$6 }' < $SWITCHLIST )
+echo name: $SW ip: $IP myip: $MYIP class: $CLASS
+if [ ! -n "$IP" ]; then
+	echo "Can't find $1"
+fi
+
+exec $THISDIR/expect-cons/$CLASS $IP $SW $MYIP ${PASSWORD/:*/} ${PASSWORD/*:/}
