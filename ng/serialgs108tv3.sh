9d24a8dc02515221453f7dc92f9c88389330e955
Ard van Breemen @ lenny <ard@kwaak.net>
2022-06-01 17:11:43 +0200

    fix curl shit

--- ng/serialgs108tv3.sh
+++ ng/serialgs108tv3.sh
@@ -1,5 +1,16 @@
 #!/bin/bash -x
 LL="$1"
+CURL_VERSION="$(curl -V|awk '{ print $2; nextfile}')"
+USE_INTERFACE=false
+USE_BRACKETS=true
+case CURL_VERSION in
+	7.35.0*)
+		USE_BRACKETS=false
+		USE_INTERFACE=false
+		;;
+esac
+
+
 ARGS="cmd=home_login&dummy=1000000000000"
 BJ4="$(echo -n "$ARGS"|md5sum|awk '{ print $1 }')"
 SCOPE="${LL/*\%25/}"
@@ -9,9 +20,14 @@ if [ "$SCOPE" = "$LL" ]; then
 		SCOPE=""
 	fi
 fi	
-if [ -n "$SCOPE" ]; then
-	INTERFACE="--interface $SCOPE"
-	LL="[${LL/\%*/}]"
+if $USE_INTERFACE; then
+	if [ -n "$SCOPE" ]; then
+		INTERFACE="--interface $SCOPE"
+		LL="${LL/\%*/}"
+	fi
+fi
+if $USE_BRACKETS; then
+	LL="[${LL}]"
 fi
 
 # netcat needs openbsd-netcat due to broken netcat in debian
